284b684e69a8f42577fcca102808e29629e776db
Phi-Dan <phiphidany@gmail.com>
2026-08-19 16:46:56 -0400

    [refactor] : Update navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,27 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import ProjectPage from './pages/ProjectPage';
+import ProjectBIRA from './pages/ProjectBIRA';
+import ProjectPODI from './pages/ProjectPODI';
 import Achievements from './pages/Achievements';
 import Team from './pages/Team';
 import Partners from './pages/Partners';
 import Contact from './pages/Contact';
-import { getProjectBySlug } from './utils/content';
 
 export default function App() {
     const [page, setPage] = useState('accueil');
     const navigate = (p: string) => setPage(p);
 
     const renderPage = () => {
-        if (page.startsWith('projet-')) {
-            const slug = page.slice('projet-'.length);
-            const project = getProjectBySlug(slug);
-            if (project)
-                return (
-                    <ProjectPage
-                        project={project}
-                        navigate={navigate}
-                    />
-                );
-        }
         switch (page) {
             case 'accueil':
                 return <Home navigate={navigate} />;
             case 'apropos':
                 return <About navigate={navigate} />;
+            case 'projet-bira':
+                return <ProjectBIRA navigate={navigate} />;
+            case 'projet-podi':
+                return <ProjectPODI navigate={navigate} />;
             case 'projets':
                 return <Projects navigate={navigate} />;
             case 'realisations':
